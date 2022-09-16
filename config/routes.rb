31b9b3f784022046aac6ca8d89b891c12d2af5da
kantatakata <112669597+kantatakata@users.noreply.github.com>
2022-09-16 15:16:39 +0000

    [5-9]

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
 root to: "homes#top"
 get '/top' => 'homes#top'
 
-get'homes/about'
+resources :post_images, only: [:new, :index, :show]
 
 
+get"/homes/about" => "homes#about",as:"about"
 
 end
